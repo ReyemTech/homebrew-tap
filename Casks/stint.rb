d8cf57e4ff06b3bc0d2137ce37199a0d2bba2d3a
Mario Meyer <mariomeyer@gmail.com>
2026-05-22 14:00:29 -0400

    feat: stint 0.1.2 (#5)
    Co-authored-by: stint-release-bot <release@reyem.tech>

--- Casks/stint.rb
+++ Casks/stint.rb
@@ -1,6 +1,6 @@
 cask "stint" do
-  version "0.1.1"
-  sha256 "dacf673038879f77b3f98752a41c5c46a2dd83a5cdea56778d686cc5512de07b"
+  version "0.1.2"
+  sha256 "88194f3a480386584f2438096e33b0dfd0d08f8566ab84e15f28a4ad6f75097e"
 
   url "https://github.com/reyemtech/stint/releases/download/v#{version}/Stint-#{version}.dmg"
   name "Stint"
